3f23dbfee352a44e0dd64b167839ff1354c399ff
Maxim Manzik <kepkpkep@gmail.com>
2023-10-02 15:39:48 +0300

    CardDeck

--- main.py
+++ main.py
@@ -484,37 +484,103 @@ from fractions import Fraction
 # print(res)
 
 
-class Cities:
-    def __init__(self):
-        self._cities = ['angl', 'ball']
-        self._index = 0
+# class Cities:
+#     def __init__(self):
+#         self._cities = ['angl', 'ball']
+#         self._index = 0
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self._index >= len(self._cities):
+#             raise StopIteration
+#         else:
+#             item = self._cities[self._index]
+#             self._index += 1
+#             return item
+#
+#
+# c = Cities()
+# for i in c:
+#     print(i)
+#
+#
+# class Cities2:
+#     def __init__(self):
+#         self._cities = ['angl', 'ball']
+#
+#     def __len__(self):
+#         return len(self._cities)
+#
+#
+# c = Cities2()
+# for i in c:
+#     print(i)
 
-    def __iter__(self):
-        return self
 
-    def __next__(self):
-        if self._index >= len(self._cities):
-            raise StopIteration
-        else:
-            item = self._cities[self._index]
-            self._index += 1
-            return item
+# s = 'sksks sksks'
+#
+# s_iter = iter(s)
+# print(next(s_iter))
+
+#
+# _SUITS = ('Spades', 'Hearts', 'Diamonds', 'Clubs')
+# _RANKS = tuple(range(2, 11)) + tuple('JQKA')
+#
+# from collections import namedtuple
+#
+# Card = namedtuple('Card', 'rank suit')
+#
+# class CardDeck:
+#     def __init__(self):
+#         self.length = len(_SUITS) * len(_RANKS)
+#
+#     def __len__(self):
+#         return self.length
+#
+#     def __iter__(self):
+#         return self.CardDeckIterator(self.length)
+#
+#     def __reversed__(self):
+#         return self.CardDeckIterator(self.length, reverse=True)
+#
+#     class CardDeckIterator:
+#         def __init__(self, length, reverse=False):
+#             self.length = length
+#             self.reverse = reverse
+#             self.i = 0
+#
+#         def __iter__(self):
+#             return self
+#
+#         def __next__(self):
+#             if self.i >= self.length:
+#                 raise StopIteration
+#             else:
+#                 if self.reverse:
+#                     index = self.length - 1 - self.i
+#                 else:
+#                     index = self.i
+#                 suit = _SUITS[index // len(_RANKS)]
+#                 rank = _RANKS[index % len(_RANKS)]
+#                 self.i += 1
+#                 return Card(rank, suit)
+#
+#
+# deck  = CardDeck()
+#
+# for card in deck:
+#     print(card)
+
+# print(list(CardDeck()))
+
+# deck_rev = reversed(deck)
+#
+# for i in deck_rev:
+#     print(i)
 
 
-c = Cities()
-for i in c:
-    print(i)
 
 
-class Cities2:
-    def __init__(self):
-        self._cities = ['angl', 'ball']
-
-    def __len__(self):
-        return len(self._cities)
-
-
-c = Cities2()
-for i in c:
-    print(i)
 
