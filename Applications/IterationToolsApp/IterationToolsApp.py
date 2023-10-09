c5e1135720ffad84ede3fb8280066101b9b50672
Maxim Manzik <kepkpkep@gmail.com>
2023-10-09 23:08:21 +0300

    IterationTollsApp step 2, 3

--- Applications/IterationToolsApp/IterationToolsApp.py
+++ Applications/IterationToolsApp/IterationToolsApp.py
@@ -1,8 +1,34 @@
+import itertools
+
 import constants
 import parse_utils
 
-for name, class_name, parser in zip(constants.names, constants.class_names, constants.parsers):
-    file_iter = parse_utils.iter_file(name, class_name, parser)
-    print(name)
-    for _ in range(3):
-        print(next(file_iter))
+# for name, class_name, parser in zip(constants.names, constants.class_names, constants.parsers):
+#     file_iter = parse_utils.iter_file(name, class_name, parser)
+#     print(name)
+#     for _ in range(3):
+#         print(next(file_iter))
+
+# gen = parse_utils.iter_combined_plain_tuple(constants.names, constants.class_names,
+#                                             constants.parsers, constants.compress_fields)
+#
+# print(list(next(gen)))
+# print(list(next(gen)))
+
+
+# nt = parse_utils.create_combo_named_tuple_class(constants.names, constants.compress_fields)
+# print(nt._fields)
+
+# data_iter = parse_utils.iter_combined(constants.names, constants.class_names,
+#                                       constants.parsers, constants.compress_fields)
+#
+# for row in itertools.islice(data_iter, 5):
+#     print(row)
+
+
+data_iter = parse_utils.filtered_iter_combined(constants.names, constants.class_names,
+                                               constants.parsers, constants.compress_fields,
+                                               key=lambda row: row.ssn == '104-84-7144')
+
+for row in itertools.islice(data_iter, 5):
+    print(row)
--- Applications/IterationToolsApp/parse_utils.py
+++ Applications/IterationToolsApp/parse_utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from collections import namedtuple
 import csv
+import itertools
 
 
 def csv_parser(file_name, *, delimiter=',', quotechar='"', include_header=False):
@@ -25,6 +26,14 @@ def create_named_tuple_class(name, class_name):
     return namedtuple(class_name, fields)
 
 
+def create_combo_named_tuple_class(names, compress_fields):
+    compress_fields = itertools.chain.from_iterable(compress_fields)
+
+    field_names = itertools.chain.from_iterable(extract_field_names(name) for name in names)
+    compress_field_names = itertools.compress(field_names, compress_fields)
+    return namedtuple('Data', compress_field_names)
+
+
 def iter_file(name, class_name, parser):
     nt_class = create_named_tuple_class(name, class_name)
     reader = csv_parser(name)
@@ -34,3 +43,29 @@ def iter_file(name, class_name, parser):
         yield nt_class(*parsed_data)
 
 
+def iter_combined_plain_tuple(names, class_names, parsers, compress_fields):
+    compress_fields = tuple(itertools.chain.from_iterable(compress_fields))
+    zipped_tuples = zip(*(iter_file(name, class_name, parser)
+                          for name, class_name, parser in zip(names, class_names, parsers)))
+
+    merged_iter = (itertools.chain.from_iterable(zipped_tuple) for zipped_tuple in zipped_tuples)
+    for row in merged_iter:
+        compressed_row = itertools.compress(row, compress_fields)
+        yield tuple(compressed_row)
+
+
+def iter_combined(names, class_names, parsers, compress_fields):
+    combo_nt = create_combo_named_tuple_class(names, compress_fields)
+    compress_fields = tuple(itertools.chain.from_iterable(compress_fields))
+    zipped_tuples = zip(*(iter_file(name, class_name, parser)
+                          for name, class_name, parser in zip(names, class_names, parsers)))
+
+    merged_iter = (itertools.chain.from_iterable(zipped_tuple) for zipped_tuple in zipped_tuples)
+    for row in merged_iter:
+        compressed_row = itertools.compress(row, compress_fields)
+        yield combo_nt(*compressed_row)
+
+
+def filtered_iter_combined(names, class_name, parsers, compress_fields, *, key=None):
+    iter_combo = iter_combined(names, class_name, parsers, compress_fields)
+    yield from filter(key, iter_combo)
